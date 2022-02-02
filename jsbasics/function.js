674f55b0c12be9ce1fc440eebad3b5fb0df8400b
shemajolivetgislain <yshemajolivet@gmail.com>
2022-02-02 12:31:57 +0200

    function in js

---
+++ jsbasics/function.js
@@ -0,0 +1,13 @@
+// performing a task
+function greet(name, address)
+{
+    console.log('Hello ' + name + ' Do you live in ' + address);
+}
+// performing calculation
+
+function calc(num)
+{
+   return num * num;
+}
+
+console.log(calc(2));
