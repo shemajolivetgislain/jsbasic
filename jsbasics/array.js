b5a9bce9dc634eabdb7d94454845e9bef0df44d2
shemajolivetgislain <yshemajolivet@gmail.com>
2022-02-03 09:51:04 +0200

    some important builtin function in array course

--- jsbasics/array.js
+++ jsbasics/array.js
@@ -1,3 +1,10 @@
 let colors = ['red','yellow','white'];
 colors[3]= 'green';
+
+// used for adding new item in array when your doesn't know your index value in your array
+colors.push('grey')
+// used for adding new item and putting to the first play
+colors.unshift('blue')
+// used for checking the index of item in array
+console.log(colors.indexOf('yellow'));
 console.log(colors);
